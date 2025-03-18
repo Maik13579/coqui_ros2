00793224df77aa15832de4add3d97b1fc161b7be
Maik Knof <knofm@hs-weingarten.de>
2025-03-18 17:50:37 +0100

    fix script

--- scripts/clone_model.py
+++ scripts/clone_model.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+from TTS.utils.manage import ModelManager
+from pathlib import Path
+
+MODEL_FILE = "/root/TTS/TTS/.models.json"
+
+import builtins
+# Automatically confirm license prompt by returning 'y' for any input call.
+builtins.input = lambda prompt='': 'y'
+
 if __name__ == '__main__':
     import sys
 
@@ -7,10 +16,8 @@ if __name__ == '__main__':
         print('Usage: ./clone_model.py <model>')
         sys.exit(1)
 
-    from TTS.api import TTS
-    import builtins
-    # Automatically confirm license prompt by returning 'y' for any input call.
-    builtins.input = lambda prompt='': 'y'
-
-    TTS(sys.argv[1])
+    model_name = sys.argv[1]
+    manager =  ModelManager(models_file=MODEL_FILE, progress_bar=True, verbose=False)
+    print(f"Downloading {model_name}")
+    model_path, config_path, model_item = manager.download_model(model_name)
     print("Done")
